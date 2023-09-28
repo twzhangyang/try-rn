dc80c0b97b4f4bcea58f3b8aa78a70c17520f993
Yang <yrzhang@thoughtworks.com>
2023-09-29 09:27:31 +1000

    integrate with auth0

--- App.tsx
+++ App.tsx
@@ -3,116 +3,96 @@
  * https://github.com/facebook/react-native
  *
  * @format
+ * @flow strict-local
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useState} from 'react';
+import {Alert, Button, StyleSheet, Text, View} from 'react-native';
+import {useAuth0, Auth0Provider} from 'react-native-auth0';
+import config from './auth0-configuration';
+import {WebAuthorizeOptions} from "react-native-auth0/lib/typescript/src/types";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Home = () => {
+  const {authorize, clearSession, user, getCredentials, error, isLoading} = useAuth0();
+  const [accessToken, setAccessToken] = useState<string | undefined>("");
+  const [idToken, setIdToken] = useState<string | undefined>("");
+  const [refreshToken, setRefreshToken] = useState<string | undefined>("");
+  // let accessToken: string | undefined, idToken: string | undefined, refreshToken: string | undefined;
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const onLogin = async () => {
+    await authorize({scope: "offline_access"}, { });
+    const credentials = await getCredentials();
+    setAccessToken(credentials?.accessToken);
+    setIdToken(credentials?.idToken);
+    setRefreshToken(credentials?.refreshToken);
   };
 
+  const loggedIn = user !== undefined && user !== null;
+
+  const onLogout = async () => {
+    await clearSession({}, {});
+  };
+
+  const Profile = () => {
+    const {user} = useAuth0();
+    return (
+        <>
+          {user &&
+              <View>
+               <Text>Logged in as {user.name}</Text>
+                <Text>Access Token: {accessToken}</Text>
+                <Text>Id Token: {idToken}</Text>
+                <Text>Refresh Token: {refreshToken}</Text>
+              </View>
+          }
+          {!user && <Text>Not logged in</Text>}
+        </>
+    )
+  }
+
+
+  if (isLoading) {
+    return <View style={styles.container}><Text>Loading</Text></View>;
+  }
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.header}> Login </Text>
+        <Profile></Profile>
+        <Button
+            onPress={loggedIn ? onLogout : onLogin}
+            title={loggedIn ? 'Log Out' : 'Log In'}
+        />
+        {error && <Text style={styles.error}>{error.message}</Text>}
+      </View>
   );
-}
+};
+
+const App = () => {
+  return (
+      <Auth0Provider domain={config.domain} clientId={config.clientId}>
+        <Home />
+      </Auth0Provider>
+  );
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  error: {
+    margin: 20,
+    textAlign: 'center',
+    color: '#D8000C'
+  }
 });
 
 export default App;
---
+++ auth0-configuration.ts
@@ -0,0 +1,6 @@
+const config = {
+    clientId: "ioy2j01UTOxQiA9ty3kLXsLJT3mD6v4O",
+    domain: "auth-yang.us.auth0.com"
+};
+
+export default config;
